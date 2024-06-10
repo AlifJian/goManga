1adf8587002554763c6350bd35bc625ed83f0b62
Alif Jian <alifjian834@gmail.com>
2024-06-10 19:52:17 +0700

    Memisahkan controller manga, dengan fitur GetMangaHome dan GetMangaImg

--- go.mod
+++ go.mod
@@ -4,10 +4,22 @@ go 1.21.4
 
 require (
 	github.com/PuerkitoBio/goquery v1.9.2 // indirect
+	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/gofiber/fiber/v2 v2.52.4 // indirect
+	github.com/google/uuid v1.5.0 // indirect
 	github.com/jung-kurt/gofpdf/v2 v2.17.3 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/yosssi/gohtml v0.0.0-20201013000340-ee4748c638f4 // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,182 +1,38 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
+	"MangaApi/manga"
 	"log"
-	"net/http"
-	"os"
-	"strconv"
-	"strings"
-	"sync"
-	"time"
 
-	"github.com/PuerkitoBio/goquery"
-	"github.com/disintegration/imaging"
-	"github.com/jung-kurt/gofpdf/v2"
+	"github.com/gofiber/fiber/v2"
 )
 
-type Manga struct {
-	Title      string
-	Indonesian bool
-	Genre      string
-	Url        string
-	ImageUrl   string
-	Id         string
-	Chapter    string
-	Uploader   string
-}
-
-func wait(seconds int, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	time.Sleep(time.Duration(seconds) * time.Second)
-	fmt.Println("Slept ", seconds, " seconds ..")
-}
-
-func Download() []byte {
-
-	fmt.Println("Start Scraping....")
-	defer fmt.Println("Download Selesai")
-	res, err := http.Get("https://mangatoto.org")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		log.Fatalf("ERROR Code: %d Status : %s", res.StatusCode, res.Status)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-
-	// html, _ := doc.Html()
-	// fmt.Println(gohtml.Format(html))
-
-	if err != nil {
-		log.Fatal("ERROR: ", err)
-	}
-
-	dataCollection := make([]Manga, 0)
-
-	doc.Find("#series-list").Children().Each(func(i int, value *goquery.Selection) {
-		isAdult := false
-		genre := ""
-		value.Find(".item-genre").Children().Each(func(i int, s *goquery.Selection) {
-			if s.Text() == "Adult" {
-				isAdult = true
-			}
-			genre += s.Text() + " , "
-		})
-
-		if !isAdult {
-			row := new(Manga)
-
-			row.Title = value.Find(".item-title").Text()
-
-			row.Genre = genre
-
-			url, _ := value.Find(".item-cover").Attr("href")
-			row.Url = "https://wto.to" + url
-
-			_, isIndonesian := value.Find(".flag-indonesia").Attr("class")
-			row.Indonesian = isIndonesian
-
-			imgUrl, _ := value.Find(".item-cover").Children().First().Attr("src")
-			row.ImageUrl = imgUrl
-
-			row.Chapter = value.Find(".item-volch").Children().First().Text()
-
-			chapterId, _ := value.Find(".item-volch").Children().First().Attr("href")
-			row.Id = strings.Split(chapterId, "chapter/")[1]
-
-			uploader, _ := value.Find(".item-volch").Children().Last().Children().First().Attr("href")
-			uploadTime := value.Find(".item-volch").Children().Last().Children().Last().Text()
-			row.Uploader = strings.Split(uploader, "/")[3] + uploadTime
-
-			dataCollection = append(dataCollection, *row)
-		}
-
-	})
-	dataJson, err := json.MarshalIndent(dataCollection, "", " ")
-
-	if err != nil {
-		log.Fatal("ERROR: ", err)
-	}
-	return dataJson
-}
+var app fiber.App = *fiber.New()
 
 func main() {
-	res, err := http.Get("https://wto.to/chapter/2829158")
 
-	if err != nil {
-		log.Fatal("ERROR: ", err)
-	}
+	app.Get("/", func(c *fiber.Ctx) error {
+		home := manga.GetMangaHome()
 
-	defer res.Body.Close()
+		return c.JSON(fiber.Map{
+			"status": "200",
+			"data":   home,
+		})
+	})
 
-	if res.StatusCode != 200 {
-		log.Fatalf("ERROR Code: %d Status : %s", res.StatusCode, res.Status)
-	}
+	app.Get("/id/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		imgChapter := manga.GetMangaImg("https://wto.to/chapter/" + id)
+		imgLength := len(*imgChapter)
 
-	doc, err := goquery.NewDocumentFromReader(res.Body)
+		return c.JSON(fiber.Map{
+			"status": "200",
+			"data": fiber.Map{
+				"imgUrl":    imgChapter,
+				"imgLength": imgLength,
+			},
+		})
+	})
 
-	if err != nil {
-		log.Fatal("ERROR: ", err)
-	}
-
-	script := doc.Find("script")
-	imgUrl := make([]string, 0)
-
-	img := strings.Split(strings.Replace((strings.Split(strings.Split(script.Text(), "imgHttps = [")[1], "];")[0]), "\"", "", -1), ",")
-
-	fmt.Println(img)
-	fmt.Println(imgUrl)
-
-	pdf := gofpdf.New("P", "mm", "A4", "")
-
-	for i, v := range img {
-		pdfWidth, pdfHeight := pdf.GetPageSize()
-
-		imgPath := string("img/" + strconv.Itoa(i) + ".jpeg")
-		file, err := os.Create(imgPath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-
-		resp, err := http.Get(v)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-
-		fmt.Println(i, " : ", v)
-		byte, _ := io.Copy(file, resp.Body)
-		fmt.Println("Ukuran File Download : ", byte)
-
-		imgFile, err := imaging.Open(imgPath, imaging.AutoOrientation(true))
-		if err != nil {
-			log.Fatal(err)
-		}
-		// var w io.Writer
-		// encErr := imaging.Encode(w, imgFile, imaging.JPEG)
-		// if encErr != nil {
-		// 	log.Fatal(encErr)
-		// }
-		imgSaveErr := imaging.Save(imgFile, imgPath)
-		if imgSaveErr != nil {
-			log.Fatal(imgSaveErr)
-		}
-		pdf.AddPage()
-
-		pdf.ImageOptions(file.Name(), 0, 0, pdfWidth, pdfHeight, false, gofpdf.ImageOptions{ImageType: "JPEG", ReadDpi: true}, 0, "")
-	}
-
-	pdferr := pdf.OutputFileAndClose("output.pdf")
-	if pdferr != nil {
-		log.Fatal(pdferr)
-	}
-
+	log.Fatal(app.Listen(":3000"))
 }
