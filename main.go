d74c185e0a179558ff2a673535c4d0331a70749b
Alif Jian <alifjian834@gmail.com>
2024-07-14 19:24:12 +0700

    Add: A request on a non-existent endpoint returns a 404

--- main.go
+++ main.go
@@ -13,5 +13,9 @@ func main() {
 	router.Route(&app)
 
 	port := util.EnvPortOr("3000")
+
+	app.Use("/", func(c *fiber.Ctx) error {
+		return c.Status(404).SendString("404 Page Not Found")
+	})
 	app.Listen(port)
 }
